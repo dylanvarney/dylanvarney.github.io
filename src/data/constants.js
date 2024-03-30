1685026adb500679299a20517a856500e5f95275
Dylan Varney <dyl@varn.xyz>
2024-03-30 16:55:23 +1000

    init

---
+++ src/data/constants.js
@@ -0,0 +1,167 @@
+export const Bio = {
+  name: "Dylan Varney",
+  roles: [
+    "Systems Administrator",
+    "Okta Certified Professional",
+  ],
+  description:
+    "I hold a Diploma in Information Technology from Griffith College, where I gained foundational knowledge and skills in IT systems and solutions. Since Nov 2022, I have been working as a Systems Administrator at a leading insurance provider, where I design and implement various identity and access management projects and processes, leveraging my core competencies in AD, IAM, SCIM, and SSO. In my current role, I have successfully delivered attribute level sourcing from multiple people systems into Active Directory, a new OU structure for AD, and SCIM integration for dozens of enterprise applications, among other initiatives. These projects have enabled the organization to streamline and automate its identity and access management, improve its security and compliance, and enhance its user experience and productivity. I work with a diverse and collaborative team of IT professionals, and I contribute to the team's goals and vision by sharing my expertise, learning from others, and supporting the continuous improvement of our systems and practices. I am motivated by the opportunity to solve complex IT challenges, and learn new technologies.",
+  github: "https://github.com/dylanvarney",
+  linkedin: "https://www.linkedin.com/in/dylan-varney/",
+};
+
+export const skills = [
+  {
+    title: "Identity and Directory",
+    skills: [
+      {
+        name: "Okta",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/okta-icon.svg",
+      },
+      {
+        name: "Entra ID",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/microsoft-azure.svg",
+      },
+      {
+        name: "Active Directory",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/microsoft-windows-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Infrastructure",
+    skills: [
+      {
+        name: "AWS",
+        image: "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/aws.svg",
+      },
+      {
+        name: "Terraform",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/terraform-icon.svg",
+      },
+      {
+        name: "Windows Server",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/microsoft-windows-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "Powershell",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/microsoft-windows-icon.svg",
+      },
+      {
+        name: "Markdown",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/markdown.svg",
+      },
+    ],
+  },
+  {
+    title: "Productivity",
+    skills: [
+      {
+        name: "Microsoft 365",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/microsoft-icon.svg",
+      },
+      {
+        name: "Google Workspaces",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/google-icon.svg",
+      },
+      {
+        name: "Slack",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/slack-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/git-icon.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/github-icon.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/visual-studio-code.svg",
+      },
+      {
+        name: "Obsidian",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/obsidian-icon.svg",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://raw.githubusercontent.com/gilbarbara/logos/main/logos/postman-icon.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "",
+    role: "Systems Administrator",
+    company: "Auto & General",
+    date: "October 2022 -",
+    desc: "Enhanced user experiences on Neurobit PSG & Hybrid, Portals by resolving bugs & reduced load time by 40%. Built Neurobit Analytics portal using React Js with seamless interaction of REST APIs using AXIOS optimized with React Query. Refactored previous code to TypeScript, updated with new dependency and integrated Vite with Jest for Unit Testing.",
+    skills: [
+      "Okta",
+      "Entra ID",
+      "Active Directory",
+      "AWS",
+      "Terraform",
+      "Azure",
+      "Cohesity",
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "",
+    school: "Griffith, Brisbane, Australia",
+    date: "Jan 2015 - Oct 2015",
+    desc: "",
+    degree: "Diploma of Information Technology",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "People Data Enhancement",
+    date: "Sep 2023 -",
+    description:
+      "Integration of HRaaS (HR as a Source) into Okta, sourcing from UKG Ready, Aurion, and Workday, configured to writeback to AD. Implementation and design of ABAC (Attribute-Based Access Control) for roles and licensing into downstream applications.",
+    image:
+      "",
+    tags: ["Okta", "SCIM", "API", "HRaaS"],
+    category: "Identity",
+    github: "",
+    webapp: "",
+  },
+];
+
